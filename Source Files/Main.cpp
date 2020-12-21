1320134d5648e0897853e10d033a8b3f79006716
Mohamed N. Gaafar <46086535+mo-gaafar@users.noreply.github.com>
2020-12-21 09:41:33 +0200

    Main Instructions Added

--- Source Files/Main.cpp
+++ Source Files/Main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 
+/*Write a main function and do the following:
+a. Create an employee object E1 and reads his data
+b. Create an employee object E2 and reads his data
+c. Create an employee object E3 and reads his data
+d. Create a department object D1 and reads its data (make its code = 10)
+e. Create a department object D2 and reads its data (make its code = 20)
+f. Make E1 join D1
+g. Make E2 join D2
+h. Make E3 join D1
+i. Print the information of E1 and E2 and E3
+j. CountEmployees that works in department code 10
+k. CountEmployees that works in department code 30
+l. Make E1 leave D1
+m. CountEmployees that works in department code 10
+n. Print the information of E1 and E2 and E3
+What is the order of creation and deletion of the classes in this exercise?*/
+
 void main()
 {
 	
