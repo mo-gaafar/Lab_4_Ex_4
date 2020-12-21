1c77f1c49a45de9e0e053224de2e194d353fc328
Mohamed N. Gaafar <46086535+mo-gaafar@users.noreply.github.com>
2020-12-21 07:43:05 +0200

    Source and Header Files

---
+++ Source Files/Department.cpp
@@ -0,0 +1,16 @@
+#include "Department.h"
+#include <iostream>
+using namespace std;
+
+Department::Department()
+{
+	cout << "Department Constructor is Called" << endl;
+
+}
+
+Department::~Department()
+{
+
+}
+
+Department
